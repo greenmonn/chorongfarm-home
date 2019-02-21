fb09c6ffc59f87a2fb5940c9206111c7da3c758a
greenmon <greenmon@kaist.ac.kr>
2019-02-21 17:10:12 +0900

    Add webpack config
    Import common CSS
    
    Import external dependencies from CDN

---
+++ vue.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+            @import "@/styles/common.scss";
+          `,
+      },
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackExternalsPlugin({
+        externals: [
+          {
+            module: 'font-awesome',
+            entry: 'https://use.fontawesome.com/releases/v5.0.10/js/all.js',
+            global: 'font-awesome',
+          },
+          {
+            module: 'daum-map-api',
+            entry:
+              '//dapi.kakao.com/v2/maps/sdk.js?appkey=c1c962956ce1fc22660080992e45295a&libraries=clusterer',
+            global: 'daum-map-api',
+          },
+        ],
+      }),
+    ],
+  },
+};
