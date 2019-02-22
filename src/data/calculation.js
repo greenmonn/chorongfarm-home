1b8b4a808324b5d819a91a483f8af8136f36d1f6
greenmon <greenmon@kaist.ac.kr>
2019-02-22 22:07:14 +0900

    Add .nvmrc
    Add .nvmrc to specify node version.

---
+++ src/data/calculation.js
@@ -0,0 +1,12 @@
+export default {
+  formatPrice(priceNumber) {
+    const converted = Array.from(priceNumber.toString());
+    let i = 1;
+    while (converted.length - 4 * i >= 0) {
+      converted.splice(converted.length - 4 * i + 1, 0, ',');
+      i += 1;
+    }
+
+    return converted.join('');
+  },
+};
