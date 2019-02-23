c63ca0b1e2520a62db323d5135bbfc70a3fbd5c2
greenmon <greenmon@kaist.ac.kr>
2019-02-23 22:40:39 +0900

    Change picture on main banner
    Change picture on main banner and alignment when screen width changes.

--- src/components/ui/Banner.vue
+++ src/components/ui/Banner.vue
@@ -1,35 +1,61 @@
 <template>
   <section class="hero">
     <figure class="image">
-      <div>최상의 품종, 최상의 환경에서 길러낸 버섯으로 <br> 여러분의 건강을 생각합니다.</div>
-      <img src="../../assets/mushroom_basket.jpg">
+      <div>최상의 품종, 최상의 환경에서 길러낸 버섯으로
+        <br>여러분의 건강을 생각합니다.
+      </div>
+      <img class="show-on-pc" src="../../assets/working_house.jpg">
+      <img class="show-on-mobile" src="../../assets/working_house_small.jpg">
     </figure>
   </section>
 </template>
 
 <style lang="scss" scoped>
 figure {
-    @media (max-width: 632px) {
-        height: 200px;
-        div {
-            display: none;
-        }
-    }
-  height: 400px;
+  height: 500px;
   overflow: hidden;
   position: relative;
 
   div {
+    position: absolute;
+    top: 200px;
+    width: 100%;
+
+    padding-left: 30px;
+    padding-right: 30px;
+
+    font-weight: 800;
+    font-size: 1.8rem;
+    color: white;
+  }
+
+  @media (max-width: 632px) {
+    height: 200px;
+
+    div {
+      display: none;
+    }
+
+    img {
+
+    }
+  }
+
+  @media (max-width: 768px) {
+    height: 300px;
+
+    div {
+      display: none;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    height: 300px;
+    div {
       position: absolute;
-      top: 200px;
-      width: 100%;
-
-      padding-left: 30px;
-      padding-right: 30px;
-
-      font-weight: 800;
-      font-size: 1.5rem;
-      color: white;
+      top: 120px;
+      font-size: 1.4rem;
+    }
   }
 
   img {
